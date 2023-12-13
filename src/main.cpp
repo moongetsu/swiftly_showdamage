2f7974b1739b56b010f4188756fcdf417391290b
moongetsu <moongetsu1@gmail.com>
2023-12-13 17:54:17 +0200

    hau

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void OnProgramLoad(const char *pluginName, const char *mainFilePath)
 
 void OnPlayerHurt(Player *player, Player *attacker, short dmgHealth, short dmgArmor, short hitgroup, const char *weapon, bool fatal)
 {
-    g_playerManager->SendMsg(HUD_PRINTTALK, FetchTranslation("showdamage.centertext"), dmgHealth, player->GetName());
+    player->SendMsg(HUD_PRINTTALK, FetchTranslation("showdamage.centertext"), dmgHealth, player->GetName());
 }
 
 void OnPluginStart()
