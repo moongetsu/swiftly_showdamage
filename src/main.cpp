c8c1f89685e38135c7c282582204f176030374cc
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2023-12-13 11:48:46 +0200

    Adding more debugs

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ void OnProgramLoad(const char *pluginName, const char *mainFilePath)
 void OnPlayerHurt(Player *player, Player *attacker, short dmgHealth, short dmgArmor, short hitgroup, const char *weapon, bool fatal)
 {
     g_playerManager->SendMsg(HUD_PRINTTALK, FetchTranslation("showdamage.centertext"), dmgHealth, player->GetName());
+    print(FetchTranslation("showdamage.centertext"), dmgHealth, player->GetName());
     print("%s took %d health from %s with weapon %s.\n", attacker->GetName(), dmgHealth, player->GetName(), weapon);
 }
 
